99568e5dc81364d9b4110811c893d6fef9a66ba1
test <serhiik91@gmail.com>
2021-11-12 10:39:09 +0200

    hw1

---
+++ javascript/classw1/clw1.js
@@ -0,0 +1,72 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+//  let result =[1,3,4,7,11,18,29,47,76,123];
+// console.log(result)
+
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book={
+//     name:'HarryPotter',
+//     page:400,
+//     genre:'fantastic'
+// }
+// console.log(book)
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let book2={
+//     name:'HarryPotter',
+//     page:600,
+//     genre:'fantastic',
+//     author:'joanRouling'
+//
+// }
+ // 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let arr=[
+//     book={
+//     name: 'Huliver',
+//     page:400,
+//     genre:'fantastic'
+// },
+//     book2={
+//         name:'HarryPotter',
+//         page:600,
+//         genre:'fantastic',
+//         author:'joanRouling'
+//
+//     },
+//     book3={
+//         name:' Lincoln the Unknown.',
+//         page:500,
+//         genre:'Biography',
+//         author:'dailCarnegie'
+//     }
+//
+//
+// ]
+//
+// console.log(arr [0] );
+// console.log(arr [1] );
+// console.log(arr [2] );
+//
+//
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+
+// let h= 23;
+// let w=10;
+// let s=h*w;
+// console.log(s);
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// const Pi = 3.14;
+// let hc= 10;
+// let dc = Math.pow(4, 2);
+// let v = Pi*dc*hc;
+// console.log(v);
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **)
+// let n = Math.pow(3,2)
+// let m = Math.pow(4, 2);
+// let k = Math.sqrt((Math.pow(3, 2) + Math.pow(4, 2)))
+// console.log(k)
---
+++ javascript/home_work/hw1.js
@@ -0,0 +1,188 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let h = 'hello';
+let o = 'owu';
+let c = 'com';
+let u = 'ua';
+let one = 1;
+let ten =10;
+let minus = -999;
+let a = 123;
+let b = 3.14;
+let d = 2.7;
+let six= 16;
+let t =true;
+let f = false;
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(h);
+// // alert(h);
+// document.write(h)
+//
+// console.log(o);
+// // alert(o);
+// document.write(o);
+//
+// console.log(u);
+// // alert(u);
+// document.write(u);
+//
+// console.log(c);
+// // alert(c);
+// document.write(c);
+//
+// console.log(one);
+// // alert(one);
+// document.write(one);
+//
+// console.log(ten);
+// // alert(ten);
+// document.write(ten);
+//
+// console.log(minus);
+// // alert(minus);
+// document.write(minus);
+//
+// console.log(a);
+// // alert(a);
+// document.write(a);
+//
+// console.log(b);
+// // alert(b);
+// document.write(b);
+//
+// console.log(d);
+// // alert(d);
+// document.write(d);
+//
+// console.log(six);
+// // alert(six);
+// document.write(six);
+//
+// console.log(t);
+// // alert(t);
+// document.write(t);
+//
+// console.log(f);
+// // alert(f);
+// document.write(f);
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+
+h = 'hallo';
+o = 'school';
+c = 'dot';
+u = 'usa';
+one = 111;
+ten = -10;
+minus = -1000;
+a = 'xxx';
+b = 'yyy';
+d = 30;
+six = 666;
+t = 'man';
+f = 'woman';
+
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(h);
+// // alert(h);
+// document.write(h)
+//
+// console.log(o);
+// // alert(o);
+// document.write(o);
+//
+// console.log(c);
+// // alert(c);
+// document.write(c);
+//
+// console.log(u);
+// // alert(u);
+// document.write(u);
+//
+// console.log(one);
+// // alert(one);
+// document.write(one);
+//
+// console.log(ten);
+// // alert(ten);
+// document.write(ten);
+//
+// console.log(minus);
+// // alert(minus);
+// document.write(minus);
+//
+// console.log(a);
+// // alert(a);
+// document.write(a);
+//
+// console.log(b);
+// // alert(b);
+// document.write(b);
+//
+// console.log(d);
+// // alert(d);
+// document.write(d);
+//
+// console.log(six);
+// // alert(six);
+// document.write(six);
+//
+// console.log(t);
+// // alert(t);
+// document.write(t);
+//
+// console.log(f);
+// // alert(f);
+// document.write(f);
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// let firstName = 'Serhii';
+// let middleName = 'Olehovich';
+// let lastName = 'Kobryn';
+// let person = firstName+' '+middleName+' '+lastName
+//
+// console.log(person)
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+
+// let p = prompt('enter your name');
+// console.log(p);
+//
+// let pr = prompt('enter your lastname');
+// console.log(pr);
+//
+// let pro = prompt('enter your age');
+// console.log(pro);
+
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// console.log(`Вітаю  ${p} ${pr} Мені ${pro} років`);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof 100);
+// console.log( typeof '100');
+// console.log(typeof true);
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 == 6 -> false
+// 5 >= 6 -> false
+// 10 == 10 -> true
+// 10 === 10 -> true
+// 10 >= 10 -> false
+// 10 <= 10 -> false
+// 10 !== 10 -> false
+// 123 ==='123' -> false
+// 123 == '123' -> true
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");205 ( 20 перевело в цифру і 5 залишилось просто) br не рахуються як number
+// document.write(str - a + "<br/>");15  ( 20 перевело  в цифру і - 5)
+// document.write(str * "2" + "<br/>"); 40 ( 20 перевело в цифру *2)
+// document.write(str / 2 + "<br/>"); 10 (20 перевело в цифру поділило на 2)
