5081299f9e3989599955db6d17f79fba3c41b629
serhii <serhiik91@gmail.com>
2021-12-29 12:30:12 +0200

    project js

--- javascript/mini-project/project.js
+++ javascript/mini-project/project.js
@@ -14,31 +14,15 @@ fetch('https://jsonplaceholder.typicode.com/users')
             userDiv.innerHTML =
                 `<h1>ID: ${user.id}</h1>
                  <h2>Name: ${user.name}</h2>`;
-            let button = document.createElement('button');
-            button.innerText = 'Details';
-            button.onclick = (id) => {
-                fetch('https://jsonplaceholder.typicode.com/users')
-                    .then(response => response.json())
-                    .then(users => {
-                        for (const item of users) {
-                            let details = document.createElement('div');
-                            details.classList.add('card2')
-                            details.innerHTML =
-                                `<h2>Email: ${item.email}</h2>
-                                 <h3>Body: ${item.body}</h3>
-                                 <h3>Adress: ${item.address.street.city.zipcode.geo}</h3>
-                                 <h3>Phone: ${item.phone}</h3>
-                                 <h4>Website:${item.website}</h4>
-                                 <h5>Company:${item.company.name.bs.catchPhrase}</h5>`;
+            let detailsList = document.createElement('a');
+            detailsList.href = 'details.html?id='+user.id
+            detailsList.innerText = 'details info'
+            detailsList.classList.add('details');
 
-                      userDiv.appendChild(details);
-                        }
-                    });
-            }
 
-            button.disabled = true;
 
-            userDiv.appendChild(button);
+
+            userDiv.appendChild(detailsList)
             wrap.appendChild(userDiv);
             document.body.appendChild(wrap);
 
